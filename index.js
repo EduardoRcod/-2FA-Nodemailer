a37eb74ef4c45fbab7eb9e346fae203005df8827
Eduardo Ramírez <53834737+EduardoRcod@users.noreply.github.com>
2024-10-28 10:05:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+//Dependencias
+const express = require('express')
+const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken');
+
+
+//Modelos
+const modelo = require("./modelo.js")
+const { error } = require('console')
+
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+
+    modelo.inicio("Cynthia",function(err, filas)
+    {
+        if (err) {
+
+            return res.status(500).json({error: "Ocurrió un error"})
+
+        } else {
+            
+            return res.json(filas);
+        }
+    })
+
+
+
+
+  //res.send('Hello World')
+})
+
+app.get('/verificar', function(req,res){
+
+    var email = req.query.email;
+    var pass = req.query.pass;
+
+    modelo.verificar(email,pass, function(err,filas){
+
+        if (err) {
+            
+            return res.status(500).json({error: "Ocurrió un error inesperado"})
+
+        } else {
+
+            return res.json(filas);
+            
+        }
+    })
+        
+})
+
+app.post('/enviarCorreo', function(req,res){
+
+    var email = req.body.email;
+    var token = req.body.token;
+
+    modelo.enviarCorreo(email,token, function(err, filas){
+        
+        if (err) {
+            
+            return res.status(500).json({error: "Ocurrió un error inesperado"})
+
+        } else {
+
+            return res.json(filas);
+            
+        }
+    })
+    
+    console.log(token);
+
+})
+
+// Nueva ruta para verificar el token
+app.get('/verificarToken', function(req, res) {
+    const token = req.query.token; // Recupera el token desde el parámetro de consulta
+
+    if (!token) {
+        return res.status(400).json({ error: 'Token no proporcionado' });
+    }
+
+    // Verifica el token con jwt.verify
+    jwt.verify(token, 'claveToken2024', (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ error: 'Token inválido o expirado' });
+        }
+        
+        // Si el token es válido, envía la respuesta con los datos decodificados
+        res.status(200).json({ message: 'Token verificado exitosamente', decoded });
+    });
+});
+
+
+app.listen(3000)
